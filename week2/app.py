f6f319622098acad2c3fb0b71e8fe58a8e0eb4bc
Syed Hammad Razi <syedhammad.razi@students.mq.edu.au>
2025-05-04 11:28:55 +0500

    Week 2: Client-Server setup

---
+++ week2/app.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template
+from flask_socketio import SocketIO
+
+app = Flask(__name__)
+socketio = SocketIO(app)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@socketio.on('connect')
+def handle_connect():
+    print('Client connected')
+
+if __name__ == '__main__':
+    socketio.run(app, debug=True)
